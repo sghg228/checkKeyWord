73bb2048e790580c48858a90020651585cd4fbcd
sghg228 <rideofthedrums@gmail.com>
2023-11-16 10:15:57 +0300

    не асинхроный клиент (работает)

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
+import threading
+
 import docx2txt
 from openpyxl import load_workbook
 import time
 import socket
 from striprtf.striprtf import rtf_to_text
-##
+
 
 def check_keywords_in_text(text, keywords):
     found_keywords = [keyword for keyword in keywords if keyword in text]
     return found_keywords
+
+
 #
 
 def check_keywords_in_docx(file_path, keywords):
@@ -23,7 +27,7 @@ def check_keywords_in_rtf(file_path, keywords):
     return check_keywords_in_text(text, keywords)
 
 
-def check_keywords_in_xlsx(file_path, keywords):
+def check_keywords_in_xlsx(file_path: str, keywords: list) -> str:
     workbook = load_workbook(file_path)
     content = []
     for sheet_name in workbook.sheetnames:
@@ -34,29 +38,44 @@ def check_keywords_in_xlsx(file_path, keywords):
     return check_keywords_in_text('\n'.join(content), keywords)
 
 
+def extension_processing(file_path, keywords):
+    found_keywords = None
+    if file_path.endswith(('.txt', '.docx', '.doc', '.rtf', 'xls', 'xlsx')):
+        if file_path.endswith('.txt'):
+            found_keywords = check_keywords_in_text(open(file_path, 'r', encoding='utf-8').read(), keywords)
+        elif file_path.endswith(('.docx', '.doc')):
+            found_keywords = check_keywords_in_docx(file_path, keywords)
+        elif file_path.endswith('.rtf'):
+            found_keywords = check_keywords_in_rtf(file_path, keywords)
+        elif file_path.endswith('.xlsx'):
+            found_keywords = check_keywords_in_xlsx(file_path, keywords)
+        return found_keywords
+
+
 def search_files_in_folder(folder_path, keywords):
     name = os.environ['USERNAME']
+    log = []
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
             start_time = time.time()
             file_path = os.path.join(root, file_name)
-            found_keywords = None
-            if file_path.endswith(('.txt', '.docx', '.doc', '.rtf', 'xls', 'xlsx')):
-                if file_path.endswith('.txt'):
-                    found_keywords = check_keywords_in_text(open(file_path, 'r', encoding='utf-8').read(), keywords)
-                elif file_path.endswith(('.docx', '.doc')):
-                    found_keywords = check_keywords_in_docx(file_path, keywords)
-                elif file_path.endswith('.rtf'):
-                    found_keywords = check_keywords_in_rtf(file_path, keywords)
-                elif file_path.endswith('.xlsx'):
-                    found_keywords = check_keywords_in_xlsx(file_path, keywords)
-                if found_keywords:
-                    print(f'[{str(name)}] File {file_path} contains keywords: {", ".join(found_keywords)}')
-            end_time = time.time()
 
+            found_keywords = extension_processing(file_path, keywords)
+            if found_keywords:
+                add_log(name, file_path, found_keywords, log)
+            end_time = time.time()
             # Рассчитайте разницу, чтобы узнать время выполнения
             execution_time = end_time - start_time
             print(str(execution_time) + ' ' + file_name)
+    return log
+
+def add_log(name, file_path, found_keywords, log):
+   log.append(f'{file_path}{", ".join(found_keywords)}')
+
+
+def background_search(folder_path, keywords):
+    search_thread = threading.Thread(target=search_files_in_folder, args=(folder_path, keywords))
+    search_thread.start()
 
 
 if __name__ == "__main__":
@@ -66,4 +85,9 @@ if __name__ == "__main__":
     # Список ключевых слов для поиска
     keywords = ['Test a']
 
-    search_files_in_folder(folder_path, keywords)
+    log = search_files_in_folder(folder_path, keywords)
+
+    print([str(x) + '\n' for x in log])
+
+
+
