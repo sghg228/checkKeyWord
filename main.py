159227ae8d498b12be608983452097030685aa6a
sghg228 <rideofthedrums@gmail.com>
2023-11-17 11:19:19 +0300

    Добавлены детекторы кодировки, осталось всего 4 ошибки. Скоро обед.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+
 import os
 import threading
 import traceback
+from chardet.universaldetector import UniversalDetector
+
 
 import docx2txt
 from openpyxl import load_workbook
@@ -42,8 +45,20 @@ def check_keywords_in_rtf(file_path, keywords):
                 with open(file_path, encoding='latin-1') as infile:
                     content = infile.read()
                     text = rtf_to_text(content)
-            except:
-                print(str('Ошибка:\n' + str(traceback.format_exc()) + ' ' + str(file_path)))
+            except Exception as e3:
+                try:
+                    detector = UniversalDetector()
+                    with open(file_path, 'r') as fh:
+                        for line in fh:
+                            detector.feed(line)
+                            if detector.done:
+                                break
+                        detector.close()
+                    found_keywords = check_keywords_in_text(open(file_path, 'r', encoding=str(detector.result["encoding"]), errors="ignore").read(), keywords)
+                except Exception as e4:
+                    print(f'Ошибка: {e4} {file_path} {detector.result["encoding"]}')
+                #print(f'Ошибка: {e3} {file_path}')
+
     return check_keywords_in_text(text, keywords)
 
 
@@ -62,15 +77,43 @@ def check_keywords_in_xlsx(file_path: str, keywords: list) -> str:
     return check_keywords_in_text('\n'.join(content), keywords)
 
 
+def check_keyword_in_txt(file_path, keywords):
+    found_keywords = None
+    try:
+        found_keywords = check_keywords_in_text(open(file_path, 'r').read(), keywords)
+    except Exception as e:
+        #print(f'Ошибка: {e} {file_path}')
+        try:
+            found_keywords = check_keywords_in_text(open(file_path, 'r', encoding="UTF-16").read(), keywords)
+        except Exception as e2:
+            try:
+                found_keywords = check_keywords_in_text(open(file_path, 'r', encoding="utf-8").read(), keywords)
+            except Exception as e3:
+
+                try:
+                    detector = UniversalDetector()
+                    with open(file_path, 'r', errors="ignore") as fh:
+                        for line in fh:
+                            detector.feed(line)
+                            if detector.done:
+                                break
+                        detector.close()
+                    found_keywords = check_keywords_in_text(open(file_path, 'r', errors="ignore", encoding=str(detector.result["encoding"])).read(), keywords)
+                except Exception as e4:
+                    print(f'Ошибка: {e3} {file_path} {detector.result["encoding"]}')
+                    k = 1
+
+
+
+
+    return found_keywords
+
+
 def extension_processing(file_path, keywords):
     found_keywords = None
     if file_path.endswith(('.txt', '.docx', '.doc', '.rtf', 'xls', 'xlsx')):
         if file_path.endswith('.txt'):
-            try:
-                found_keywords = check_keywords_in_text(open(file_path, 'r').read(), keywords)
-            except Exception as e:
-                k = 1
-               #  print(str('Ошибка:\n' + str(traceback.format_exc()) + ' ' + str(file_path) + ' КОД ' + str(e)))
+            found_keywords = check_keyword_in_txt(file_path, keywords)
         elif file_path.endswith(('.docx', '.doc')):
             found_keywords = check_keywords_in_docx(file_path, keywords)
         elif file_path.endswith('.rtf'):
@@ -88,7 +131,7 @@ def search_files_in_folder(folder_path, keywords):
         for file_name in files:
 
             file_path = os.path.join(root, file_name)
-            if file_path.endswith(('.zip', '.rar', '.tar')):
+            if file_path.endswith(('.zip', '.rar', '.tarz')):
                 continue
             else:
                 found_keywords = extension_processing(file_path, keywords)
@@ -108,8 +151,7 @@ def add_log(name, file_path, found_keywords, log):
 
 if __name__ == "__main__":
     # Путь к папке, которую нужно проверить
-    folder_path = 'C:\\'
-
+    folder_path = 'D:\\'
     # Список ключевых слов для поиска
     keywords = ['steam']
 
@@ -122,3 +164,7 @@ end_time = time.time()
 # Рассчитайте разницу, чтобы узнать время выполнения
 execution_time = end_time - start_time
 print(str(execution_time))
+
+
+
+######## интерфейс, регистр, фоновый режим , формат лога, сокеты, автозапуск
